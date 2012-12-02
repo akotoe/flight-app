19b399e083b6dc031fce480af354e840718cb211
Edward Akoto <edward.akoto@sv.cmu.edu>
2012-12-02 15:45:32 -0800

    Initial Commit

---
+++ FlightApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FlightApp
+//
+//  Created by Edward Akoto on 12/2/12.
+//  Copyright (c) 2012 Edward Akoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FlightApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FlightApp
+//
+//  Created by Edward Akoto on 12/2/12.
+//  Copyright (c) 2012 Edward Akoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
